a05020c7cc2ebe75eae776264c8c0bf12d3ffd2d
Tim Jarratt <tjarratt@gmail.com>
2014-01-25 10:34:22 -0800

    Tests should clean themselves up
    No use having a bunch of generated files cluttering up the fixtures dir.
    
    Might be nice to actually force this to happen in a clean temp directory, but
    in the mean time, this isn't so bad.

--- main_test.go
+++ main_test.go
@@ -1,33 +1,29 @@
 package main_test
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"io/ioutil"
 	"os"
-  "os/exec"
+	"os/exec"
 	"path/filepath"
-  . "github.com/onsi/ginkgo"
-  . "github.com/onsi/gomega"
+	"regexp"
 )
 
 func init() {
-  Describe("using ginkgo-converter", func() {
-		BeforeEach(func() {
-			cwd, err := os.Getwd()
-			Expect(err).NotTo(HaveOccurred())
+	Describe("using ginkgo-converter", func() {
+		BeforeEach(killAllConvertedGinkgoTests)
+		AfterEach(killAllConvertedGinkgoTests)
 
-			cmd := exec.Command("rm", filepath.Join(cwd, "fixtures/xunit_ginkgo_test.go"))
-			cmd.Run()
-		})
-
-    It("rewrites xunit tests as ginkgo tests", func() {
+		It("rewrites xunit tests as ginkgo tests", func() {
 			cwd, err := os.Getwd()
 			Expect(err).NotTo(HaveOccurred())
 
 			pathToExecutable := filepath.Join(cwd, "ginkgo-convert")
 
-      cmd := exec.Command(pathToExecutable, "github.com/tjarratt/ginkgo-convert/fixtures")
+			cmd := exec.Command(pathToExecutable, "github.com/tjarratt/ginkgo-convert/fixtures")
 			err = cmd.Run()
-      Expect(err).NotTo(HaveOccurred())
+			Expect(err).NotTo(HaveOccurred())
 
 			convertedFile, err := ioutil.ReadFile(filepath.Join(cwd, "fixtures/xunit_ginkgo_test.go"))
 			Expect(err).NotTo(HaveOccurred())
@@ -35,6 +31,26 @@ func init() {
 			goldMaster, err := ioutil.ReadFile(filepath.Join(cwd, "fixtures/ginkgo_test_goldmaster.go"))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(string(convertedFile)).To(Equal(string(goldMaster)))
-    })
-  })
+		})
+	})
+}
+
+func killAllConvertedGinkgoTests() {
+	cwd, err := os.Getwd()
+	Expect(err).NotTo(HaveOccurred())
+
+	fixtures := filepath.Join(cwd, "fixtures")
+	dirContents, err := ioutil.ReadDir(fixtures)
+	Expect(err).NotTo(HaveOccurred())
+
+	regex := regexp.MustCompile(".+_ginkgo_test.go")
+	for _, file := range dirContents {
+		if !regex.MatchString(file.Name()) {
+			continue
+		}
+
+		pathToFile := filepath.Join(fixtures, file.Name())
+		err = os.Remove(pathToFile)
+		Expect(err).NotTo(HaveOccurred())
+	}
 }
